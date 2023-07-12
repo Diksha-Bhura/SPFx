859f7c7fd3b5e0ded872ebec00a91ae5981b2361
Diksha Bhura <dikshabhurak@gmail.com>
2023-07-12 15:30:17 -0400

    Provision List from spfx solution

---
+++ webparts/react-PasswordStorer/src/webparts/storePassword/loc/mystrings.d.ts
@@ -0,0 +1,18 @@
+declare interface IStorePasswordWebPartStrings {
+  PropertyPaneDescription: string;
+  BasicGroupName: string;
+  DescriptionFieldLabel: string;
+  AppLocalEnvironmentSharePoint: string;
+  AppLocalEnvironmentTeams: string;
+  AppLocalEnvironmentOffice: string;
+  AppLocalEnvironmentOutlook: string;
+  AppSharePointEnvironment: string;
+  AppTeamsTabEnvironment: string;
+  AppOfficeEnvironment: string;
+  AppOutlookEnvironment: string;
+}
+
+declare module 'StorePasswordWebPartStrings' {
+  const strings: IStorePasswordWebPartStrings;
+  export = strings;
+}
